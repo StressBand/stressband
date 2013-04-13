7a32ee8aff996119eefb86d9e07d298c2cc852a5
Wesley Lauka <wlauka@gmail.com>
2013-04-13 00:49:01 -0400

    added finale state and replay

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -3,10 +3,10 @@
 File:		mobile.js
 
 Title:		StressBand Moble Display Module
-Date:		3 / 24 / 2012
+Date:			3 / 24 / 2012
 Author:		Wesley Lauka
  
-Desc:		The client-side display and mini-game logic for stressband's mobile component
+Desc:			The client-side display and mini-game logic for stressband's mobile component
 
 ----------------------------------------------- */
 
@@ -18,9 +18,9 @@ SB.mobile = (function($,_,createjs,d3){
 			$container	:"",
 			$screen		:""
 		},
-		stage,breaths = [],breathing,fps = 30,blowfish,
+		stage,breaths = [],breathing,fps = 30,blowfish,fishBaseY = 30,
 		// function list
-		init,setupScreen,manageAnimations, updateStage,inflateBlowfish,logBreaths,graph;
+		init,setupScreen,manageAnimations,updateStage,getRandomY,inflateBlowfish,logBreaths,endGame,graph;
 
 	/*  setupScreen - Initialize the Elements of the Mini-Game
 		----------------------------------------------- */
@@ -28,31 +28,39 @@ SB.mobile = (function($,_,createjs,d3){
 		stage = new createjs.Stage(jQMap.$screen[0]);
 		createjs.Ticker.useRAF = true;
 		createjs.Ticker.setFPS(fps);
-		
+
 		// get spritesheet data
 		$.getJSON('images/mobile/blowfishAnim_003.json',function(data){
 			blowfish = new createjs.BitmapAnimation(new createjs.SpriteSheet(data));
 			blowfish.gotoAndPlay('s1_idle');
 			blowfish.state = 1;
 			blowfish.x = Math.floor(jQMap.$screen.width()/2) - 184;
-			blowfish.y = 30;
+			blowfish.y = fishBaseY;
 			stage.addChild(blowfish);
-			
+
 			// start the game loop
-			createjs.Ticker.addEventListener('tick',updateStage)
+			createjs.Ticker.addEventListener('tick',updateStage);
 			// inflate the fish!
 			inflateBlowfish(4);
-			manageAnimations('undulate',{to:Math.sin(createjs.Ticker.getTime())*20}); // move up and down
+			manageAnimations('undulate',{to:getRandomY()}); // move up and down
 			manageAnimations('roll',{to:1}); // rotate a little
 		});
 	};
-	
+
 	/*  updateStage - Main Mobile Game Loop
 	----------------------------------------------- */
 	updateStage = function(){
 		stage.update();
 	};
-	
+
+	/*  getRandomY - Generate Random Y Delta, Positive or Negative
+	----------------------------------------------- */
+	getRandomY = function(){
+		var dir = Math.random() < 0.5 ? -1 : 1;
+		var val = fishBaseY + ( Math.floor( Math.random() * 20 ) * dir );
+		return val;
+	};
+
 	/*  manageAnimations - Deal With the Blowfish's Animations
 		args:	anim		- animation to start
 				param{}		- animation parameters
@@ -60,8 +68,8 @@ SB.mobile = (function($,_,createjs,d3){
 	manageAnimations = function(anim,param){
 		switch (anim) {
 			case 'undulate':
-				// fix y movement
-				createjs.Tween.get(blowfish).to({y:blowfish.y+param.to},1000,createjs.Ease.sineInOut).call(function(){ 
+				createjs.Tween.get(blowfish).to({y:param.to},1000,createjs.Ease.sineInOut).call(function(){
+					manageAnimations('undulate',{to:getRandomY()});
 				});
 				break;
 			case 'roll':
@@ -70,16 +78,19 @@ SB.mobile = (function($,_,createjs,d3){
 				});
 				break;
 			case 'updateY':
-				createjs.Tween.get(blowfish).to({y:blowfish.y+50},1000,createjs.Ease.sineInOut).call(function(){
-					manageAnimations('undulate',{to:(Math.sin(createjs.Ticker.getTime())*20)});
-				});
+				fishBaseY = blowfish.y + param.delta; // all you have to do is change the base!
 				break;
 			case 'finale':
+				createjs.Tween.get(blowfish,{override:true}).to({y:blowfish.y-40},1500,createjs.Ease.bounceIn).call(function(){
+					createjs.Tween.get(blowfish,{override:true}).to({y:$(window).height()},4000,createjs.Ease.backInOut).call(function(){
+						endGame();
+					});
+				});
 				break;
 		}
-		
+
 	};
-	
+
 	/*  updateStage - Change the blowfish's state
 		args:	state		- the state to transition to
 		----------------------------------------------- */
@@ -103,15 +114,15 @@ SB.mobile = (function($,_,createjs,d3){
 					inflateBlowfish(state);
 				});
 			}
-		} // END - return function
+		}; // END - return function
 	})();
-	
+
 	/*  logBreaths - Check sensor readings and record successful breaths
 		args:	reading		- data point's differential from stretch sensor
 		----------------------------------------------- */
 	logBreaths = function(reading){
 		// change these to alter sensitivity
-		var peak = .85, valley = .1;
+		var peak = 0.85, valley = 0.1;
 		if(reading > peak) { breathing = true; console.log('breath'); }
 		if(reading < valley && breathing){
 			console.log('breath over');
@@ -127,20 +138,20 @@ SB.mobile = (function($,_,createjs,d3){
 				}
 			}
 		}
-	}
-	
+	};
+
 	/* PUBLIC init - Module Initialization
-		args:	container 	- the main html element of the view, ID ref
-				canvas		- the canvas element for holding the mini-game, ID ref
+		args:	container		- the main html element of the view, ID ref
+					canvas			- the canvas element for holding the mini-game, ID ref
 	----------------------------------------------- */
 	init = function(container, canvas){
 		jQMap.$container = $(container);
 		jQMap.$screen = $(canvas);
-		jQMap.$screen[0].width = $(window).width(); 
-		jQMap.$screen[0].height = $(window).height(); 
-		
+		jQMap.$screen[0].width = $(window).width();
+		jQMap.$screen[0].height = $(window).height();
+
 		setupScreen();
-		
+
 		// moving through the prompts
 		$('#prompt .card .next').on('click',function(e){
 			if($(this).closest('.card').is(':last-child')){ // last prompt. start the game
@@ -158,14 +169,36 @@ SB.mobile = (function($,_,createjs,d3){
 			}
 
 		});
-		$('#cancel').on('click',function(e){ window.close() });
-	
+
+		$('#cards').on('click','.cancel',function(e){ window.close(); });
+
 		// are we in debug mode?
 		if(window.location.search.indexOf('debug') !== -1){
-			drawChart();	
-		}		
+			drawChart();
+		}
 	};
-	
+
+	/* endGame - Deal with Post Game Response 
+	----------------------------------------------- */
+	endGame = function(){
+		var $endCard = $('#endCard li').eq(0).clone();
+		$endCard.appendTo('#cards');
+		$('#prompt').fadeIn(500);
+		$endCard.on('click','.restart',function(e){
+			$('#cards').animate({'left':-$(window).width()+'px'},function(){ // jump to severity card
+				$endCard.remove();
+				// reset the game
+				blowfish.y = fishBaseY = 30;
+				$('#breaths li').removeClass('lit');
+				breaths = [];
+				inflateBlowfish(4);
+
+			});
+			return false;
+		});
+
+	};
+
 	//-------------------------------- CHARTING FUNCTIONS ( FOR DEBUG ONLY )
 	var chart,data,line,x;
 	function drawChart(){
@@ -176,37 +209,36 @@ SB.mobile = (function($,_,createjs,d3){
 			readings: [],
 			freq: 0
 		};
-		
+
 		// Set up the Chart
 		var container = document.getElementById('chart'),
 			width = 250,
 			height = 90,
 			margin = [10,10,10,10];
-			
+
 		chart = d3.select(container).append('svg')
 			.attr("width",width)
 			.attr("height",height);
-		
-		
-		x = d3.scale.linear().domain([0,80]).range([0, width]);	
-		var y = d3.scale.linear()	
+
+		x = d3.scale.linear().domain([0,80]).range([0, width]);
+		var y = d3.scale.linear()
 			.domain([data.max,data.min])
-			.range([0+margin[2],height-margin[0]]);	
+			.range([0+margin[2],height-margin[0]]);
 		var yAxis = d3.svg.axis()
 			.scale(y).orient('right');
 		chart.append('g').attr('class', 'grid').call(yAxis.tickSize(width,-50,0).tickFormat(""));
-		
-		
+
+
 		line = d3.svg.line()
 			.x(function(d,i){ return x(i+80); })
 			.y(function(d){ return y(d); })
 			.interpolate("basis");
-			
+
 		chart.append("g").attr("id", "graph")
 			.append("path")
 			.attr("id","line")
-			.attr("d",line(data.readings))
-		
+			.attr("d",line(data.readings));
+
 		// Clipping Mask
 		chart.append("svg:clipPath")
 			.attr("id","clip")
@@ -218,7 +250,7 @@ SB.mobile = (function($,_,createjs,d3){
 			.attr("transform","translate(0,"+(margin[0])+")");
 		d3.select('#graph').attr("clip-path", "url(#clip)");
 	}
-	
+
 	function updateChart(point){
 		data.readings.push(point);
 		chart.select("#line")
@@ -229,9 +261,10 @@ SB.mobile = (function($,_,createjs,d3){
 			.attr("transform", "translate(" + -x(data.readings.length) + ")");
 	}
 	//-------------------------------- END CHARTING FUNCTIONS ( FOR DEBUG ONLY )
-	
+
 	return {
-		init:init
+		init:init,
+		anim:manageAnimations
 	};
-	
+
 })(jQuery,_,createjs,d3);
